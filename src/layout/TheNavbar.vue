1fe5c34aa133acca1e0f29a6d941319554da6e63
saman1945 <saman.alaii10@gmail.com>
2022-09-28 01:49:08 +0330

    navbar

---
+++ src/layout/TheNavbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app-bar app color="blue-grey darken-4" dark>
+    <div class="d-flex align-sta">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+    <v-btn target="_blank" text>
+      <span class="mr-2">Login/Signup</span>
+    </v-btn>
+    <v-btn target="_blank" text>
+      <span class="mr-2">Contact</span>
+    </v-btn>
+    <v-btn target="_blank" text>
+      <span class="mr-2">Product</span>
+    </v-btn>
+    <v-btn target="_blank" text>
+      <span class="mr-2">Home</span>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import './assets/css/app.css'
+import './assets/css/color.css'
+
 
 Vue.config.productionTip = false
 
